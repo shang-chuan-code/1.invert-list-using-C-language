8026112bbfb7d318aedec834d49984959a3479e5
shangchuan <shangchuan97@163.com>
2025-04-21 22:28:12 +0800

    [ADD]添加翻转列表结构

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * @Author: shangchuan shangchuan97@163.com
+ * @Date: 2025-04-21 22:17:04
+ * @LastEditors: shangchuan shangchuan97@163.com
+ * @LastEditTime: 2025-04-21 22:18:11
+ * @FilePath: \1.invert-list-using-C-language\main.c
+ */
+#include <stdio.h>
+#include "list.h"
+
+
+
+
+
+int main()
+{
+    printf("hello\n");
+}
