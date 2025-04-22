f03bd451cb0fc6a3df39e43550cd900d4dc519f4
shangchuan <shangchuan97@163.com>
2025-04-22 22:10:44 +0800

    [CHANGE]列表增加尾部元素，不用每个尾差

--- main.c
+++ main.c
@@ -2,17 +2,77 @@
  * @Author: shangchuan shangchuan97@163.com
  * @Date: 2025-04-21 22:17:04
  * @LastEditors: shangchuan shangchuan97@163.com
- * @LastEditTime: 2025-04-21 22:18:11
+ * @LastEditTime: 2025-04-22 22:08:45
  * @FilePath: \1.invert-list-using-C-language\main.c
  */
 #include <stdio.h>
-#include "list.h"
+#include <stdlib.h>
 
 
 
+typedef struct node
+{
+    int data;
+    struct node *next;
+}Node;
+
+
+Node * list_add_tail(Node **tail, int data)
+{
+    Node *node = (Node *)malloc(sizeof(Node));
+    node->data = data;
+    node->next = NULL;
+
+    if(NULL == *tail)
+    {
+        *tail = node;
+    }
+    else
+    {
+        (*tail)->next = node;
+        *tail = node;
+    }
+    return *tail;
+}
+
+Node* list_add_tail_v2(Node *tail, int data)
+{
+    Node *node = (Node *)malloc(sizeof(Node));
+    node->data = data;
+    node->next = NULL;
+    
+    tail->next = node;
+    return tail->next;
+}
+
+
+void printf_list(Node* head)
+{
+    if(NULL == head)
+    {
+        printf("head is NULL\n");
+    }
+    else
+    {
+        Node* tmp = head;
+        while (NULL != tmp)
+        {
+            printf("list data = %d\n", tmp->data);
+            tmp = tmp->next;
+        }
+    }
+    
+}
 
 
 int main()
 {
-    printf("hello\n");
+    Node *head = NULL;
+    Node *tail = NULL;
+    tail = list_add_tail(&head, 1);
+    tail = list_add_tail(&tail, 2);
+    tail = list_add_tail(&tail, 3);
+    tail = list_add_tail(&tail, 3);
+    printf_list(head);
+
 }
